6695ae2d9c08aa9dede33ae17e2605f563797459
Christopher Nsikak <christophernsikak1@gmail.com>
2024-02-16 09:15:54 +0100

    Fully Working javascript

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,18 @@
-document.querySelectorAll(".button-collapse").forEach((e) => e.addEventListener("click", toggleCollapse));
-let cardContentStyle = document.querySelector(".card-content").style;
-let squareIcon = document.querySelector(".fa-plus-square").style;
-let minusIcon = document.querySelector(".fa-minus-square").style;
+document.querySelectorAll(".card").forEach((element) => {
+  element.addEventListener("click", toggleCollapse, { capture: true });
+});
 
 function toggleCollapse(ev) {
-    if (cardContentStyle.display === "none") {
-        cardContentStyle.display = "block";
-        squareIcon.visibility = "hidden";
-        minusIcon.visibility = "visible";
-    } else {
-        cardContentStyle.display = "none";
-        squareIcon.visibility = "visible";
-        minusIcon.visibility = "hidden";
-
-    }
+  let cardContentStyle = this.querySelector(".card-content").style;
+  let squareIcon = this.querySelector(".fa-plus-square").style;
+  let minusIcon = this.querySelector(".fa-minus-square").style;
+  if (cardContentStyle.display === "none" || cardContentStyle.display === "" ) {
+    cardContentStyle.display = "block";
+    squareIcon.visibility = "hidden";
+    minusIcon.visibility = "visible";
+  } else {
+    cardContentStyle.display = "none";
+    squareIcon.visibility = "visible";
+    minusIcon.visibility = "hidden";
+  }
 }
